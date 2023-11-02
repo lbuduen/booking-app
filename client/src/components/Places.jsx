b64cb078389e034a973476df7467f5341c8b21f2
Leodan A Buduen <lbuduendev@gmail.com>
2023-11-02 16:22:00 -0400

    client photo uploading

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,9 +1,15 @@
+import { useRef, useState } from 'react'
 import { Link, useParams } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from 'yup';
+import axios from 'axios'
+
+import Perk from '../components/Perk'
 
 export default function Places() {
   const { action } = useParams()
+  const photoLinkRef = useRef(null)
+  const [photos, setPhotos] = useState([])
 
   const formik = useFormik({
     initialValues: {
@@ -21,6 +27,37 @@ export default function Places() {
       console.log(values);
     }
   })
+
+  const uploadPhotoLink = async () => {
+    const url = photoLinkRef.current.value
+    if (url) {
+      try {
+        const res = await axios.post('/api/v1/place/upload-photo-link', { photoLink: url })
+        setPhotos(prev => [...prev, res.data.filename])
+        photoLinkRef.current.value = ''
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
+  const uploadPhotoFiles = async (evt) => {
+    const files = evt.target.files
+    const data = new FormData()
+    for (const file of files) {
+      data.append('photos', file)
+    }
+    try {
+      const res = await axios.post("/api/v1/upload/photos", data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      setPhotos(prev => [...prev, ...res.data.filenames])
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <div>
@@ -47,16 +84,24 @@ export default function Places() {
           <label htmlFor="photoLink" className="inline-block text-xl mt-4">Photos</label>
           <p className="text-gray-500 text-xs">more = better</p>
           <div className="flex gap-2">
-            <input type="text" id="photoLink" placeholder="Add using a link" {...formik.getFieldProps("photoLink")} />
-            <button className="bg-gray-200 px-4 rounded-2xl w-32">Add photo</button>
+            <input type="text" ref={photoLinkRef} placeholder="Add using a link" />
+            <button type="button" onClick={uploadPhotoLink} className="bg-gray-200 px-4 rounded-2xl w-32">Add photo</button>
           </div>
-          <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-            <button className="flex gap-1 items-center justify-center border bg-transparent rounded-2xl p-6 text-2xl text-gray-600">
+          <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+            {
+              photos && photos.map((photo, i) => (
+                <div className='h-32' key={i}>
+                  <img className='rounded-2xl' src={`http://localhost:5000/uploads/${photo}`} alt="" />
+                </div>
+              ))
+            }
+            <label className="h-32 flex gap-1 items-center justify-center border bg-transparent rounded-2xl p-6 text-2xl text-gray-600">
+              <input type="file" multiple onChange={uploadPhotoFiles} className='hidden' />
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
               </svg>
               Upload
-            </button>
+            </label>
           </div>
 
           <label htmlFor="description" className="inline-block text-xl mt-4">Description</label>
@@ -66,41 +111,36 @@ export default function Places() {
           <label className="inline-block text-xl mt-4">Perks</label>
           <p className="text-gray-500 text-xs">select all the perks of your place</p>
           <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-              <input type="checkbox" />
+            <Perk>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z" />
               </svg>
               <span>WiFi</span>
-            </label>
-            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-              <input type="checkbox" />
+            </Perk>
+            <Perk>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
               </svg>
               <span>Free parking spot</span>
-            </label>
-            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-              <input type="checkbox" />
+            </Perk>
+            <Perk>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
               </svg>
               <span>TV</span>
-            </label>
-            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-              <input type="checkbox" />
+            </Perk>
+            <Perk>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
               </svg>
               <span>Pets</span>
-            </label>
-            <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-              <input type="checkbox" />
+            </Perk>
+            <Perk>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
               </svg>
               <span>Private entrance</span>
-            </label>
+            </Perk>
           </div>
 
           <label htmlFor="extraInfo" className="inline-block text-xl mt-4">Extra info</label>
